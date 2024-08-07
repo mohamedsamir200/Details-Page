3e3985702710fd44721561cae7bdf020cb702412
mohamedsamir200 <ms8113453@gmail.com>
2024-08-08 00:04:05 +0300

    Add Delete and update Reviews

--- src/Components/Reviewss/Reviewss.jsx
+++ src/Components/Reviewss/Reviewss.jsx
@@ -1,12 +1,22 @@
 import React, { useEffect, useState } from "react";
-import { Rating, Button, Label } from "flowbite-react";
+import { Rating, Button, Label, Alert, Modal, TextInput ,Textarea } from "flowbite-react";
 import db from "../../Config/firebase";
-import { addDoc, collection, onSnapshot } from "firebase/firestore";
+import { addDoc, collection, deleteDoc, doc, onSnapshot, setDoc, updateDoc } from "firebase/firestore";
+import { HiInformationCircle } from "react-icons/hi";
+import { MdDelete } from "react-icons/md";
+import { FaPenToSquare } from "react-icons/fa6";
 
 function Reviewss() {
   const [titleInput, setTitleInput] = useState("");
   const [descInput, setDescInput] = useState("");
   const [reviewList, setReviewList] = useState([])
+  const [showAlert, setShowAlert] = useState(false);
+  const [currentId, setCurrentId] = useState("");
+
+  const [openModal, setOpenModal] = useState(false);
+  const [updateTitle, setUpdateTitle] = useState("");
+  const [updateDesc, setUpdateDesc] = useState("");
+  
   useEffect(() => {
     showReviews()
   }, [])
@@ -14,15 +24,23 @@ function Reviewss() {
   // ============ Add Reviews To Database =================//
   async function addToDb() {
     if (titleInput == "" || descInput == "") {
-      alert("Please Fill All Inputs")
+      // alert("Please Fill Inputs")
+      setShowAlert(true); // Show alert when inputs are not filled
     } else {
       const collectionRef = collection(db, "reviews");
       const doc = await addDoc(collectionRef, { title: titleInput, desc: descInput });
       setTitleInput("");
       setDescInput("");
+      setShowAlert(false); // Hide alert after successful submission
+
     }
 
   }
+
+  // Close the alert
+  const handleCloseAlert = () => {
+    setShowAlert(false);
+  };
 
 
   // ============ Show Reviews From Database  =================//
@@ -30,80 +48,109 @@ function Reviewss() {
   function showReviews() {
     onSnapshot(collection(db, "reviews"), (snapshot) => {
       let rev = snapshot.docs.map((doc) => {
+        setCurrentId(doc.id)
         return { ...doc.data(), id: doc.id }
       })
       setReviewList(rev)
     })
   }
 
+  // ============ Delete Reviews From Database  =================//
+  function deleteReview() {
+    const docRef = doc(db, "reviews", currentId);
+    deleteDoc(docRef);
+  }
+
+  // ============ Update Reviews From Database  =================//
+  // 1- set value To Edit
+  function editReview( currentTitle, currentDesc) {
+    setUpdateTitle(currentTitle);
+    setUpdateDesc(currentDesc);
+    setOpenModal(true);
+  }
+  //2- Update Value
+  function updateReview() {
+    const docRef = doc(db, "reviews", currentId);
+
+    updateDoc(docRef, { title: updateTitle , desc : updateDesc});
+    setOpenModal(false);
+  
+  }
+
+
   return (
     <>
-      <div className="reviews ">
-        <div className="review   mx-auto space-y-4">
-          <h1 className="text-[#344054] font-bold">Customer Feedback</h1>
-          <div className="rate grid  zeroToTo768:grid-cols-1 from768:grid-cols-3 gap-5  m-auto">
-            <div className="product-rate bg-white p-5 rounded-lg flex justify-center items-center flex-col">
-              <h1 className="text-bold text-7xl text-[#164C96]">48</h1>
-              <div className="rate-icon">
-                <i class="fa-solid fa-star text-yellow-400"></i>
-                <i class="fa-solid fa-star text-yellow-400"></i>
-                <i class="fa-solid fa-star text-yellow-400"></i>
-                <i class="fa-solid fa-star text-yellow-400"></i>
-                <i class="fa-solid fa-star text-yellow-400"></i>
-              </div>
-              <p>Product Rating</p>
+      <div className="review   mx-auto space-y-4">
+        <h1 className="text-[#344054] font-bold">Customer Feedback</h1>
+        <div className="rate grid  zeroToTo768:grid-cols-1 from768:grid-cols-3 gap-5  m-auto">
+          <div className="product-rate bg-white p-5 rounded-lg flex justify-center items-center flex-col">
+            <h1 className="text-bold text-7xl text-[#164C96]">48</h1>
+            <div className="rate-icon">
+              <i class="fa-solid fa-star text-yellow-400"></i>
+              <i class="fa-solid fa-star text-yellow-400"></i>
+              <i class="fa-solid fa-star text-yellow-400"></i>
+              <i class="fa-solid fa-star text-yellow-400"></i>
+              <i class="fa-solid fa-star text-yellow-400"></i>
             </div>
-            <div className="progress bg-white p-7 rounded-lg col-span-2">
-              <div>
-                <Rating.Advanced
-                  percentFilled={70}
-                  className={`mb-2 flex justify-center test `}
-                ></Rating.Advanced>
-                <Rating.Advanced
-                  percentFilled={17}
-                  className="mb-2 flex justify-center"
-                ></Rating.Advanced>
-                <Rating.Advanced
-                  percentFilled={8}
-                  className="mb-2 flex justify-center"
-                ></Rating.Advanced>
-                <Rating.Advanced
-                  percentFilled={4}
-                  className="mb-2 flex justify-center"
-                ></Rating.Advanced>
-                <Rating.Advanced
-                  percentFilled={1}
-                  className=" flex justify-center"
-                ></Rating.Advanced>
-              </div>
+            <p>Product Rating</p>
+          </div>
+          <div className="progress bg-white p-7 rounded-lg col-span-2">
+            <div>
+              <Rating.Advanced
+                percentFilled={70}
+                className={`mb-2 flex justify-center test `}
+              ></Rating.Advanced>
+              <Rating.Advanced
+                percentFilled={17}
+                className="mb-2 flex justify-center"
+              ></Rating.Advanced>
+              <Rating.Advanced
+                percentFilled={8}
+                className="mb-2 flex justify-center"
+              ></Rating.Advanced>
+              <Rating.Advanced
+                percentFilled={4}
+                className="mb-2 flex justify-center"
+              ></Rating.Advanced>
+              <Rating.Advanced
+                percentFilled={1}
+                className=" flex justify-center"
+              ></Rating.Advanced>
             </div>
           </div>
+        </div>
 
-          <h1 className="text-[#344054] font-bold">Reviews</h1>
-          <div className="testimonial border-b border-gray-400 pb-5 px-5">
+        <h1 className="text-[#344054] font-bold">Reviews</h1>
+        <div className="testimonial border-b border-gray-400 pb-5 px-5">
 
-            {reviewList.map((item) =>
+          {reviewList.map((item) =>
 
-              <div key={item.id}>
+            <div key={item.id}>
+              <div className="flex items-center justify-between ">
                 <h3 className="mt-4 text-gray-700 font-bold">{item.title}</h3>
-                <div className="testi-icon mb-2">
-                  <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
-                  <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
-                  <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
-                  <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
-                  <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
+                <div className="flex space-x-3 text-2xl cursor-pointer">
+                  <MdDelete className="text-red-700" title="delete" onClick={deleteReview} />
+                  <FaPenToSquare className="text-slate-700" title="update" onClick={()=>editReview(item.title, item.desc)} />
+
                 </div>
+              </div>
 
-                <p className="text-gray-500">
-                  {item.desc}
-                </p>
+              <div className="testi-icon mb-2">
+                <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
+                <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
+                <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
+                <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
+                <i class="fa-solid fa-star text-yellow-400 text-[10px]"></i>
+              </div>
 
-              </div>)}
+              <p className="text-gray-500">
+                {item.desc}
+              </p>
 
+            </div>)}
+        </div>
 
-          </div>
-
-
+        <div className="reviews ">
 
           <div className="writeReview ">
             <h3 className="text-gray-700 font-bold">
@@ -121,7 +168,12 @@ function Reviewss() {
                 <div className="mb-2 block">
                   <Label htmlFor="text" value="Review Title" />
                 </div>
-                <input value={titleInput} type="text" className="w-full bg-transparent rounded-lg mb-5 " onChange={(e) => setTitleInput(e.target.value)} />
+                {showAlert && (
+                  <Alert color="failure" icon={HiInformationCircle} onDismiss={handleCloseAlert}>
+                    <span className="font-medium">Info alert!</span> Please Fill Inputs
+                  </Alert>
+                )}
+                <input value={titleInput} type="text" className="w-full bg-transparent rounded-lg mb-5 mt-2" onChange={(e) => setTitleInput(e.target.value)} />
               </div>
 
               <div>
@@ -135,6 +187,46 @@ function Reviewss() {
           </div>
         </div>
       </div>
+
+      <Modal
+        show={openModal}
+        size="md"
+        popup
+        onClose={() => setOpenModal(false)}
+      >
+        <Modal.Header />
+        <Modal.Body>
+          <div className="space-y-6">
+            <div className="title">
+              <Label>Title :</Label>
+              <TextInput
+                type="text"
+                value={updateTitle}
+                onChange={(e) => setUpdateTitle(e.target.value)}
+              />
+            </div>
+
+            <div className="Desc">
+              <Label>Desc :</Label>
+              <Textarea 
+              required rows={10}
+                type="text"
+                value={updateDesc}
+                onChange={(e) => setUpdateDesc(e.target.value)}
+              />
+            </div>
+
+            <div className="w-full">
+              <Button
+                className="btn bg-yellow-400"
+                onClick={() => updateReview()}
+              >
+                Update
+              </Button>
+            </div>
+          </div>
+        </Modal.Body>
+      </Modal>
     </>
   );
 }
